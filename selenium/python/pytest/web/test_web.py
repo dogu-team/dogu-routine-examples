08de9cd2b200d5bdc7329004ecd139ff9e934ee3
oneofthezombies <hunhoekim@gmail.com>
2023-08-30 03:52:20 +0900

    wip: selenium chrome

--- selenium/python/pytest/web/test_web.py
+++ selenium/python/pytest/web/test_web.py
@@ -4,12 +4,3 @@ from selenium.webdriver.common.by import By
 
 def test_go_to_dogutech_io(driver: WebDriver):
     driver.get("https://dogutech.io/")
-
-
-def test_search_for_dogu(driver: WebDriver):
-    global dogu_elements
-    driver.find_elements(By.XPATH, '//*[contains(text(), "dogu")]')
-
-
-def test_check_for_dogu():
-    assert len(dogu_elements) > 0
