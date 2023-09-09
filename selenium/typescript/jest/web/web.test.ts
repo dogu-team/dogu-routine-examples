346fc4985dd4ed0fa5910e2034a6598316210b62
oneofthezombies <hunhoekim@gmail.com>
2023-09-09 23:15:36 +0900

    chore: increase timeout

--- selenium/typescript/jest/web/web.test.ts
+++ selenium/typescript/jest/web/web.test.ts
@@ -1,9 +1,13 @@
 import { test, expect } from "@jest/globals";
 import { driver } from "./setup.js";
 
-test("go to dogutech.io", async () => {
-  await driver.get("https://dogutech.io");
-});
+test(
+  "go to dogutech.io",
+  async () => {
+    await driver.get("https://dogutech.io");
+  },
+  5 * 60_000
+);
 
 test("find Dogu elements", async () => {
   const doguElements = await driver.findElements({
