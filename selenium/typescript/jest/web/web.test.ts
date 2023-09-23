8f367b821bb87b50bab61726ce66b4ce9ca2012d
Daniel Ahn <andoune0719@gmail.com>
2023-09-23 19:51:06 +0900

    chore: update web testing

--- selenium/typescript/jest/web/web.test.ts
+++ selenium/typescript/jest/web/web.test.ts
@@ -16,11 +16,61 @@ test("find Dogu elements", async () => {
   expect(doguElements.length).toBeGreaterThan(0);
 });
 
-test("scroll pages", async () => {
-  await driver.sleep(10000);
-  await driver.executeScript("window.scrollTo(0, document.body.scrollHeight)");
-  await driver.sleep(3000);
+test('Feature test', async () => {
+  await driver.actions().click(await driver.findElement({xpath: '//button[@id="header-book-demo-button"]'})).perform();
+  await driver.sleep(1000);
+  // scroll to button
+  await driver.executeScript("arguments[0].scrollIntoView();", await driver.findElement({xpath: '//button[@id="main-feature-execution-button"]'}));
+  await driver.actions().click(await driver.findElement({xpath: '//button[@id="main-feature-execution-button"]'})).perform();
+  await driver.sleep(1000);
+  await driver.actions().click(await driver.findElement({xpath: '//button[@id="main-feature-report-button"]'})).perform();
+  await driver.sleep(1000);
+  await driver.actions().click(await driver.findElement({xpath: '//button[@id="main-feature-integration-button"]'})).perform();
+  await driver.sleep(1000);
+})
 
-  await driver.executeScript("window.scrollTo(0, 0)");
-  await driver.sleep(3000);
-});
+test('demo test', async () => {
+  // await driver.actions().click(await driver.findElement({xpath: '//button[@id="main-bottom-book-demo-button"]'})).perform();
+  await driver.sleep(1000);
+  await driver.actions().click(await driver.findElement({xpath: '//input[@id="lastname"]'})).perform();
+  await driver.actions().sendKeys('Dogu').perform();
+  await driver.sleep(1000);
+  await driver.actions().click(await driver.findElement({xpath: '//input[@id="firstname"]'})).perform();
+  await driver.actions().sendKeys('Technologies').perform();
+  await driver.sleep(1000);
+  await driver.actions().click(await driver.findElement({xpath: '//input[@id="email"]'})).perform();
+  await driver.actions().sendKeys('test@dogutech.io').perform();
+  await driver.sleep(1000);
+  await driver.actions().click(await driver.findElement({xpath: '//input[@id="category"]'})).perform();
+  await driver.sleep(1000);
+  const elems = await driver.findElements({xpath: '//div[@class="ant-select-item ant-select-item-option ant-select-item-option-active"]'});
+  await driver.actions().click(elems[0]).click().perform();
+  await driver.sleep(1000);
+  await driver.actions().click(await driver.findElement({xpath: '//textarea[@id="message"]'})).perform();
+  await driver.actions().sendKeys('test').perform();
+  await driver.sleep(1000);
+  await driver.actions().click(await driver.findElement({xpath: '//button[@type="submit"]'})).perform();
+})
+
+// test('demo test', async () => {
+//   // await driver.actions().click(await driver.findElement({xpath: '//button[@id="main-bottom-book-demo-button"]'})).perform();
+//   await driver.sleep(1000);
+//   await driver.actions().click(await driver.findElement({xpath: '//input[@id="lastname"]'})).perform();
+//   await driver.actions().sendKeys('Dogu').perform();
+//   await driver.sleep(1000);
+//   await driver.actions().click(await driver.findElement({xpath: '//input[@id="firstname"]'})).perform();
+//   await driver.actions().sendKeys('Technologies').perform();
+//   await driver.sleep(1000);
+//   await driver.actions().click(await driver.findElement({xpath: '//input[@id="email"]'})).perform();
+//   await driver.actions().sendKeys('test@dogutech.io').perform();
+//   await driver.sleep(1000);
+//   await driver.actions().click(await driver.findElement({xpath: '//input[@id="category"]'})).perform();
+//   await driver.sleep(1000);
+//   const elems = await driver.findElements({xpath: '//div[@class="ant-select-item ant-select-item-option ant-select-item-option-active"]'});
+//   await driver.actions().click(elems[0]).click().perform();
+//   await driver.sleep(1000);
+//   await driver.actions().click(await driver.findElement({xpath: '//textarea[@id="message"]'})).perform();
+//   await driver.actions().sendKeys('test').perform();
+//   await driver.sleep(1000);
+//   await driver.actions().click(await driver.findElement({xpath: '//button[@type="submit"]'})).perform();
+// })
